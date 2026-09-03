587b933af8fae8ee36b170fdc2d416941d1b1d5f
415-23-koranya <56411@yupparaj.ac.th>
2026-09-03 07:38:54 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("🛒 แอปพลิเคชันคำนวณราคาสินค้ารวม VAT 7%")
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 vat = price * 0.07
-net_price = price + vat  # แก้ไขจากเครื่องหมาย - เป็น + เพื่อรวม VAT เข้าไปในราคาสินค้า
+net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ : {net_price:.2f} บาท")
 st.divider()
